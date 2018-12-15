d5d77e4f736c0828dbf5601011026cf66a13f14a
chlyr <charilaoslir@gmail.com>
2018-12-15 00:02:41 -0500

    Test reworked to properly show compound path

--- tests/prac2.py
+++ tests/prac2.py
@@ -10,50 +10,36 @@ sys.path.append(pwd + '/../utils')
 import numpy as np
 from rots import get_rotmat
 from rotplot import plotrot as pltr
+from rotplot import minRotMat
 import matplotlib.pyplot as plt
 
 
-# Plot incremental rotation
-
 # Axis of rotation
-s = np.array([0, 1, 0])
+s1 = np.array([0, 1, 0])
 
-# Compound rotation around s (in degrees)
+# Second rotation
+s2 = np.array([0, 0, 1])
+
+# Rotation angle (in degrees) around s1 and s2
 theta = 45
 
 # Set an initial vector
 v0 = np.array([1, 0, 1])*1/np.sqrt(2)
 
-# Discretize theta in n part
+# Decompose theta in n (~small) rotations
 n = 15
-
-# Get rotation matrix and axial matrix for dtheta
-dtheta = theta/n
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 # plot
-v0 = pltr(s, theta, v0, n, ax, d2flag=False)
+v1 = pltr(s1, theta, v0, n, ax, d2flag=False)
 
-# Second rotation
-s = np.array([0, 0, 1])
-v0 = pltr(s, theta, v0, n, ax, d2flag=False)
+v2 = pltr(s2, theta, v1, n, ax, d2flag=False)
 
-# rotate initial vector v0 to v and compare results
-sz = np.array([1, 0, 1])*1/np.sqrt(2)
-v1 = np.array([1, 1, 0])*1/np.sqrt(2)
+# get compound axis of rotation, rotation angle and rotation matrix
+s3,theta_c,R = minRotMat(v0,v2)
 
-# compound axis of rot
-t = np.cross(v0, v1)
-s = t/np.linalg.norm(t)
-
-# get angle between vectors v0-v1
-sintheta = np.dot(v0, v1)/(np.linalg.norm(v0)*np.linalg.norm(v1))
-theta = 2*np.arcsin(sintheta)*180/np.pi
-dtheta = theta/n
-r, S = get_rotmat(s, dtheta)
-
-v0 = pltr(s, theta, v0, n, ax, col='b')
+v0 = pltr(s3, theta_c, v0, n, ax, col='b')
 
 plt.show()
